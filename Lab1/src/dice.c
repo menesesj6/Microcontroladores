78c871f2a0667fd2bc2311bfb66a8ed419098803
Jorge Meneses <j.menesesgarro@gmail.com>
2025-03-22 17:14:32 -0600

    Lab1 - LFSR function fix

--- Lab1/src/dice.c
+++ Lab1/src/dice.c
@@ -1,6 +1,8 @@
 #include <pic14/pic12f683.h>
 #include <stdint.h>
 
+#define WAIT 500
+
 // Deactivate Watchdog Timer
 typedef unsigned int word;
 word __at 0x2007 __CONFIG = (_WDTE_OFF); 
@@ -10,54 +12,55 @@ word __at 0x2007 __CONFIG = (_WDTE_OFF);
 *  --------------------
 */ 
 void delay (unsigned int time);
-uint8_t lfsr (uint8_t start, uint8_t min,uint8_t max);
+uint8_t lfsr ();
+
+// Initial dice value => Seed for LFSR
+volatile uint8_t throw = 0x9F;
 
 void main (){
     // Set GPIO pins functionality, GP3 is strictly input
     TRISIO = 0b00100000; 
     // Initialize outputs in 0
-	GPIO = 0x00; 
-    
-    // Initial value of dice
-    uint8_t throw = 0x9F;
+	GPIO = 0x00;
 
     // Infinite loop
     while (1){
         // Function if dice is thrown (signal goes up in GP5)
         if (GP5) {
+
             // Generate random number
-            throw = lfsr(throw, 1, 6);
+            uint8_t roll = lfsr();
 
             // Modify GPIO outputs based on number to output on LEDs
-            switch (throw){
+            switch (roll){
                 case 1: 
                     GPIO = 0x02;
-                    delay(500);
+                    delay(WAIT);
                     GPIO = 0x00; 
                     break;
                 case 2:
                     GPIO = 0x01; 
-                    delay(500);
+                    delay(WAIT);
                     GPIO = 0x00; 
                     break;
                 case 3:
                     GPIO = 0x06; 
-                    delay(500);
+                    delay(WAIT);
                     GPIO = 0x00; 
                     break;
                 case 4:
                     GPIO = 0x11; 
-                    delay(500);
+                    delay(WAIT);
                     GPIO = 0x00; 
                     break;
                 case 5:
                     GPIO = 0x13; 
-                    delay(500);
+                    delay(WAIT);
                     GPIO = 0x00; 
                     break;
                 case 6:
                     GPIO = 0x15; 
-                    delay(500);
+                    delay(WAIT);
                     GPIO = 0x00; 
                     break;
                 default:
@@ -74,10 +77,12 @@ void delay (unsigned int time){
     }
 }
 
-uint8_t lfsr (uint8_t start, uint8_t min, uint8_t max){
-    while (1){
-        uint8_t bit = ((start >> 7) ^ (start >> 6) ^ (start >> 5) ^ (start >> 4) ^ (start >> 3)) & 1;
-        start = (start << 1) | bit;
-        return (start % 6) + 1;
+uint8_t lfsr (){
+    uint8_t feedback_pol = 0xB8;
+    uint8_t lsb = throw & 1;
+    throw >>= 1; 
+    if (lsb) {
+        throw ^= feedback_pol; 
     }
+    return throw % 6 + 1;
 }
